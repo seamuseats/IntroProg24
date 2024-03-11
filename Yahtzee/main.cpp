50dba4e9eea91d23193caf237500c155e37ea51d
Plinker <plinker@Bill-2.en.wikipedia.org>
2024-03-11 13:32:49 -0400

    working hard or hardly working (semi-functional Yahtzee)

--- Yahtzee/main.cpp
+++ Yahtzee/main.cpp
@@ -189,8 +189,32 @@ Array<bool> calcOpts(Array<int> dice, Array<bool> used){
 
 int smallStraight(Array<int> dice){
     sort(dice.asVector().begin(), dice.asVector().end());
-
+    int counts = 0;
     
+    for (int i = 0; i < dice.size(); i++){
+        if ((dice[i] == dice[i+1] - 1) && (dieStacks(dice)[2] >= 1)){
+            counts += 1;
+        }
+        if (counts >= 3){
+            return 30;
+        }
+    }
+    return 0;
+}
+
+int largeStraight(Array<int> dice){
+    sort(dice.asVector().begin(), dice.asVector().end());
+    int counts = 0;
+    
+    for (int i = 0; i < dice.size(); i++){
+        if ((dice[i] == dice[i+1] - 1) && (dieStacks(dice)[2] >= 1)){
+            counts += 1;
+        }
+        if (counts >= 4){
+            return 40;
+        }
+    }
+    return 0;
 }
 
 int calcUpperScore(Array<int> dice, int ruleNum){
@@ -253,18 +277,27 @@ int fourOfAKind(Array<int> dice){
     return 0;
 }
 
-int fullHouse(Array<int>dice){
+int fullHouse(Array<int> dice){
     Array<int> stacks = dieStacks(dice);
     for (int i = 0; i < dice.size(); i++){
         if(contains(dice, 3) && contains(dice, 2)){
             return 25;
         }
-        return 0;
+        
     }
+    return 0;
+}
+
+int yahtzee(Array<int> dice){
+    for (int i = 0; i < dice.size(); i++){
+        if(dieStacks(dice)[i] >= 5){
+            return arraySum(dice);
+        }
+    }
+    return 0;
 }
 
 int calcScore(Array<int> dice, int ruleNum){
-    int score;
     switch(ruleNum){
     // aces
     case 1:
@@ -280,7 +313,17 @@ int calcScore(Array<int> dice, int ruleNum){
     case 8:
         return fourOfAKind(dice);
     case 9:
-        break;
+        return fullHouse(dice);
+    case 10:
+        return smallStraight(dice);
+    case 11:
+        return largeStraight(dice);
+    case 12:
+        return yahtzee(dice);
+    case 13: 
+        return arraySum(dice);
+    default:
+        return 0;
     
     
 
@@ -308,37 +351,113 @@ Array<int> diceRoll(Array<int> holdId, Array<int> prevRoll){
     }
     return exportArray;
 }
-int main(){
 
-    println("{}", fullHouse({1, 1, 2, 2, 2}));
-    // string uname{getlogin()};
-    // Array<int> dice{0};
-    // Array<int> holdId{0};
-    // Array<bool> used{false, false, false, false, false, false, false, false, false, false, false, false, false};
-    // int q{0};
-    // string c;
-    // dice = diceRoll(holdId, dice);
-    // fmt::print(fg(fmt::color::green), "Welcome {} to the wonderful game: Yahtzee! Brought to you by MSSM IntroProg\n", uname);
-    // println("To begin, we'll start by rolling the dice!");
-    // // StdAudio.play("diceRoll.wav");
+string gameLoop( Array<bool> used, int& score){
+    // StdAudio.play("diceRoll.wav");
+    Array<int> dice{diceRoll({0, 0, 0, 0, 0}, {0, 0, 0, 0, 0})};
+    println("survey says...\n {}!", dice);
+    Array<int> holdId;
+    println("do you wish to reroll? (Y/n)");
+    string q;
+    cin >> q;
+    if (q == "Y" || q == "" || q == "y"){
+        println("what dice to hold? {}", dice);
+        for (int i = 0; i < dice.size(); i++){
+            int hold;
+            println("hold {}? (1 for yes 0 for no)", dice[i]);
+            cin >> hold;
+            holdId.append(hold);
+        }
+        dice = diceRoll(holdId, dice);
+        println("survey says...\n {}!", dice);
+        println("do you wish to reroll? (Y/n)");
+        cin >> q;
+        if (q == "Y" || q == "" || q == "y"){
+                println("what dice to hold? ({})", dice);
+            for (int i = 0; i < dice.size(); i++){
+                int hold;
+                println("hold {}? (1 for yes 0 for no)", dice[i]);
+                cin >> hold;
+                holdId.append(hold);
+            }
+            dice = diceRoll(holdId, dice);
+            println("survey says...\n {}!", dice);
+        }
+    }
+
+    println("what do you want to do? (0 to see what goes where)");
+    int c;
+    cin >> c;
+    if(c == 0){
+        println("(1-6, ones through sixes), (7-8, three/four of a kind), (9, full house), (10-11 sm/lg straight) (12, Yahtzee)");
+    }
+    cin >> c;
+    score += calcScore(dice, c);
+}
+
+void scoreSheet(Array<int> scores, int ruleNum){
+    print("____________\n")
+    println("|aces|{}|", )
+}
+
+int main(){
+    string uname{getlogin()};
+    Array<int> dice{0};
+    Array<int> holdId{};
+    Array<bool> used{false, false, false, false, false, false, false, false, false, false, false, false, false};
+    Array<int> scores;
+    scores.resize(13);
+    
+    dice = diceRoll(holdId, dice);
+    fmt::print(fg(fmt::color::green), "Welcome {} to the wonderful game: Yahtzee! Brought to you by MSSM IntroProg\n", uname);
+    println("To begin, we'll start by rolling the dice!");
+    // StdAudio.play("diceRoll.wav");
     // println("survey says...\n {}!", dice);
-    // println("your Options are {}", calcOpts(dice, used));
     // Array<bool> Opts{calcOpts(dice, used)};
     // println("do you wish to reroll? (Y/n)");
+    // string q;
+    int score{0};
     // cin >> q;
-    // if (q = ("Y" || "")){
+    // if (q == "Y" || q == "" || q == "y"){
+    //     println("what dice to hold? ({})", dice);
+    //     for (int i = 0; i < dice.size(); i++){
+    //         int hold;
+    //         println("hold {}? (1 for yes 0 for no)", dice[i]);
+    //         cin >> hold;
+    //         holdId.append(hold);
+    //     }
     //     dice = diceRoll(holdId, dice);
     //     println("survey says...\n {}!", dice);
-    //     println("your Options are {}", calcOpts(dice, used));
-    // }
-    // else{
-    //     println("what do you want to do? (--help to see what goes where)");
-    //     cin >> c;
-    //     if(c == "--help"){
-    //         println("(1-6, ones through sixes), (7-8, three/four of a kind), (9, full house), (10-11 sm/lg straight) (12, Yahtzee)");
+    //     println("do you wish to reroll? (Y/n)");
+    //     cin >> q;
+    //     if (q == "Y" || q == "" || q == "y"){
+    //             println("what dice to hold? {}", dice);
+    //         for (int i = 0; i < dice.size(); i++){
+    //             int hold;
+    //             println("hold {}? (1 for yes 0 for no)", dice[i]);
+    //             cin >> hold;
+    //             holdId.append(hold);
+    //         }
+    //         dice = diceRoll(holdId, dice);
+    //         println("survey says...\n {}!", dice);
     //     }
-    //     else{
-            
-    //     }
     // }
+
+    // println("what do you want to do? (0 to see what goes where)");
+    // int c;
+    // cin >> c;
+    // if(c == 0){
+    //     println("(1-6, ones through sixes), (7-8, three/four of a kind), (9, full house), (10-11 sm/lg straight) (12, Yahtzee)");
+    // }
+    // cin >> c;
+    // score = calcScore(dice, c);
+    // println("current score is now {}", score);
+    for (int i = 0; i < 12; i++){
+        fmt::print(fg(fmt::color::red), "Pretend to hear dice when you see this\n");
+        // StdAudio.play("diceRoll.wav");
+        gameLoop(used, score);
+        println("current score is now {}", score);
+    }
+    return score;
+
 }
